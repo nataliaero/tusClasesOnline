570e32570329970aa67b9129833aee6736091380
Natalia Ortiz <natorgom@gmail.com>
2020-08-23 15:35:02 +0200

    add search butto

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MESSAGES } from '../../messages';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,21 @@ import { MESSAGES } from '../../messages';
       <div class="main-info">
         <h1 class="title">{{ msg.findBestTutors }}</h1>
         <h2 class="subtitle">{{ msg.startToday }}</h2>
+        <div class="search-group">
+          <form [formGroup]="searchForm">
+            <mat-form-field class="search-form">
+              <input
+                matInput
+                name="search"
+                [formControl]="searchFormControl"
+                [placeholder]="msg.searchQuestion"
+              />
+            </mat-form-field>
+          </form>
+          <button mat-button class="search-button" (click)="onSearch()">
+            {{ msg.findTutor }}
+          </button>
+        </div>
       </div>
     </div>
   `,
@@ -15,7 +31,21 @@ import { MESSAGES } from '../../messages';
 })
 export class HomeComponent {
   msg = {
+    findTutor: MESSAGES['basic.findTutor'],
     findBestTutors: MESSAGES['home.findBestTutors'],
     startToday: MESSAGES['home.startToday'],
+    searchQuestion: MESSAGES['home.searchQuestion'],
   };
+
+  searchForm: FormGroup = new FormGroup({
+    search: new FormControl('', []),
+  });
+
+  get searchFormControl(): FormControl {
+    return this.searchForm.get('search') as FormControl;
+  }
+
+  onSearch(): void {
+    console.log(this.searchFormControl.value);
+  }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, MatButtonModule, MatInputModule, ReactiveFormsModule],
   providers: [],
 })
 export class HomeModule {}
