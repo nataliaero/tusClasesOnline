ce454937569c3e475739a4acb3ab4cd27a425de1
Natalia Ortiz <natorgom@gmail.com>
2020-09-01 21:12:56 +0200

    feat(app/home): add subjects

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
   selector: 'app-home',
   template: `
     <div class="background-image">
-      <div class="main-info">
+      <div class="header-info">
         <h1 class="title">{{ msg.findBestTutors }}</h1>
         <h2 class="subtitle">{{ msg.startToday }}</h2>
         <div class="search-group">
@@ -29,6 +29,47 @@ import { Observable } from 'rxjs';
           </button>
         </div>
       </div>
+    </div>
+    <div class="main-body">
+      <div class="subjects">
+        <div class="subject">
+          <div><mat-icon>details</mat-icon></div>
+          <div><h2>Matemáticas</h2></div>
+        </div>
+        <div class="subject">
+          <mat-icon>filter_vintage</mat-icon>
+          <h2>Física</h2>
+        </div>
+        <div class="subject">
+          <mat-icon>create</mat-icon>
+          <h2>Lengua</h2>
+        </div>
+        <div class="subject">
+          <mat-icon>laptop</mat-icon>
+          <h2>Informática</h2>
+        </div>
+        <div class="subject">
+          <mat-icon>blur_on</mat-icon>
+          <h2>Economía</h2>
+        </div>
+        <div class="subject">
+          <mat-icon>spa</mat-icon>
+          <h2>Francés</h2>
+        </div>
+        <div class="subject">
+          <mat-icon>publicon</mat-icon>
+          <h2>Inglés</h2>
+        </div>
+        <div class="subject">
+          <mat-icon>school</mat-icon>
+          <h2>Álgebra</h2>
+        </div>
+        <div class="subject">
+          <mat-icon>exposure</mat-icon>
+          <h2>Química</h2>
+        </div>
+      </div>
+      <div></div>
     </div>
   `,
   styleUrls: ['./home.component.scss'],
